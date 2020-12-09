5fac236afdf7295f97550e7f24099987ed8e3ead
Josh <joshualholland@gmail.com>
2020-12-09 17:40:05 -0600

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+## Description
+This is a basic Next.js with React boilerplate. The `Styled-component` library has been imported for styling. 
+
+## Running the app
+* Install yarn
+* Clone the repository
+* cd `boilerplate`
+* `yarn dev` 
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Home = () => (
+  <Heading>Hello World!</Heading>
+)
+
+const Heading = styled.h1`
+  font-weight: 500;
+  max-width: 1080px;
+  margin: 0 auto;
+`
+
+export default Home
